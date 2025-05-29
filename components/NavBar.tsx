ebc4f124ce0578b195bca15f1d1ebccd1f150029
Rayhan Sahaji <rayhansahajirimon@gmail.com>
2025-05-30 00:12:10 +0600

    Creating Personal Blog Site.

---
+++ components/NavBar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <nav className='flex '>
+        <ul>
+            <li></li>
+        </ul>
+    </nav>
+  )
+}
+
+export default NavBar
---
+++ config/site.ts
@@ -0,0 +1,6 @@
+const SiteConfig = {
+    "title": "Rayhan Sahaji Blog",
+    "description": "This is My Programming Blog.",
+}
+
+export default SiteConfig
