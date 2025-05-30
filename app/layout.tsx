b1534a46c9a08c3fbb62f0bec3293ee98134cadf
Rayhan Sahaji <rayhansahajirimon@gmail.com>
2025-05-30 11:41:40 +0600

    Fix tailwindcss problem and add navbar and logo.

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter as FontSans,} from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import SiteConfig from "@/config/site";
+import NavBar from "@/components/NavBar";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -26,7 +27,9 @@ export default function RootLayout({
 	      "min-h-screen bg-background font-sans antialiased",
 	      fontSans.variable
       )}
-      >{children}</body>
+      >
+        <NavBar/>
+        {children}</body>
     </html>
   );
 }
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import Link from 'next/link'
 
 const NavBar = () => {
   return (
-    <nav className='flex '>
+    <nav className='h-16 bg-background/60 sticky top-0 border-b'>
+      <div>
+        Rayhan Sahaji Blog
+      </div>
         <ul>
-            <li></li>
+            <li><Link href={"/"}>Home</Link></li>
+            <li><Link href={"/about"}>About</Link></li>
         </ul>
     </nav>
   )
